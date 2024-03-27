f1edf116fae234047b72d0b698c794ca0ac2633b
ztarbug <kronprinzfriedrich@gmx.de>
2024-03-27 14:31:52 +0100

    add java lib to generated code

---
+++ api/decision/generators/java-lib/generate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+function main {
+    case "$1" in
+        clean)
+            clean
+        ;;
+        generate)
+            generate
+        ;;
+    esac
+}
+
+function clean() {
+    mvn clean
+
+    # kick out old generated code
+    rm -r aicockpit-decision-jar/src/main/java    
+}
+
+function generate {
+    echo "generate code"
+    mvn clean package
+
+    echo "move generated code"
+    # make sure target folder exists
+    mkdir -p aicockpit-decision-jar/src/main/java
+    cp -r -f target/generated-sources/openapi/src/main/java/de/starwit aicockpit-decision-jar/src/main/java/de
+    cp target/generated-sources/openapi/src/main/java/de/JSON.java aicockpit-decision-jar/src/main/java/de
+    cp target/generated-sources/openapi/src/main/java/de/ApiException.java aicockpit-decision-jar/src/main/java/de
+}
+
+main $1
